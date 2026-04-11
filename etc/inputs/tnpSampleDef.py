ef1e8850fddd9e81e65df099c2250d8e3b600ac8
Pei-Zhu Lai <ht910155@gmail.com>
2026-04-11 18:19:37 +0200

    fixed the wrong lumi

--- etc/inputs/tnpSampleDef.py
+++ etc/inputs/tnpSampleDef.py
@@ -77,12 +77,12 @@ eoszmmg = '/eos/home-p/pelai/HZa/root_P2Root/run3_tnp_zmmg'
 Run3_2022preEE_zmmg = {
         'DY_MC_NLO_2022preEE'    : tnpSample('DY_MC_NLO_2022preEE',    eoszmmg + '/mc/DYJetsToLL/2022preEE.root', isMC=True, nEvts = 203271136),
         'DY_MC_LO_2022preEE'     : tnpSample('DY_MC_LO_2022preEE',     eoszmmg + '/mc/DYJetsToLL_MLM/2022preEE.root', isMC=True, nEvts = 202883601),
-        'Data_2022preEE'         : tnpSample('Data_2022preEE',         eoszmmg + '/Data_tnp_zmmg/2022preEE.root', lumi = 108.95),
+        'Data_2022preEE'         : tnpSample('Data_2022preEE',         eoszmmg + '/Data_tnp_zmmg/2022preEE.root', lumi = 7.98),
         }
 Run3_2022postEE_zmmg = {
         'DY_MC_NLO_2022postEE'   : tnpSample('DY_MC_NLO_2022postEE',   eoszmmg + '/mc/DYJetsToLL/2022postEE.root', isMC=True, nEvts = 203271136),
         'DY_MC_LO_2022postEE'    : tnpSample('DY_MC_LO_2022postEE',    eoszmmg + '/mc/DYJetsToLL_MLM/2022postEE.root', isMC=True, nEvts = 202883601),
-        'Data_2022postEE'        : tnpSample('Data_2022postEE',        eoszmmg + '/Data_tnp_zmmg/2022postEE.root', lumi = 108.95),
+        'Data_2022postEE'        : tnpSample('Data_2022postEE',        eoszmmg + '/Data_tnp_zmmg/2022postEE.root', lumi = 26.67),
         }
 Run3_2023preBPix_zmmg = {
         'DY_MC_NLO_2023preBPix'  : tnpSample('DY_MC_NLO_2023preBPix',  eoszmmg + '/mc/DYJetsToLL/2023preBPix.root', isMC=True, nEvts = 203271136),
